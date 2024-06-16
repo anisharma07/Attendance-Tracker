4fcc0dac0b82ad76dee92f7e2487767d18d204a4
Anirudh Sharma <anis42390@gmail.com>
2024-06-16 22:08:03 +0530

    Project Directory Created

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Landing from '@/Components/Landing';
+
+export default function Home() {
+  return (
+    <div className='w-full min-h-[100svh]'>
+      <Landing/>
+    </div>
+      
+  );
+}
