e413cb0ba141acb89c5a28cf4c968328cc171597
Anirudh Sharma <anis42390@gmail.com>
2024-08-01 22:49:10 +0530

    tailwind bug in app fixed

--- src/Components/Home/Modal.tsx
+++ src/Components/Home/Modal.tsx
@@ -12,10 +12,10 @@ const Modal: React.FC<ModalProps> = ({ isOpen, setOpen }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed top-0 left-0 z-[100s] inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed top-0 left-0 z-[100] inset-0 flex items-center justify-center bg-black bg-opacity-50">
       <div
         onClick={() => setOpen(false)}
-        className="absolute top-0 left-0 bg-black bg-opacity-50"
+        className="absolute top-0 left-0 z-[100] bg-black bg-opacity-50"
       ></div>
       <div className="p-6 rounded-lg shadow-lg w-80 bg-[#1F222B]">
         <h2 className="text-lg font-semibold text-foreground">Add Subject</h2>
